271e0bd896aa543d3639c6d5a569c3545fedf828
gourjoy <goursaha307@gmail.com>
2023-01-26 14:17:53 +0600

    pushing

---
+++ README.md
@@ -0,0 +1,1 @@
+check the live site here 👉 https://gsjoy24.github.io/gsjoy-portfolio/
---
+++ others/js.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	$('.btn').click(function () {
+		$(this).text('Request Sent');
+		$(this).addClass('btn-success');
+	});
+});
